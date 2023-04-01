c90351349dae9047e9217d806d65caa60d346217
Justin Muller <wickedjust@hotmail.com>
2023-04-01 14:49:03 -0700

    setup simple express server

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+//e.g server.js
+import express from 'express';
+import ViteExpress from 'vite-express';
+
+const app = express();
+
+app.get("/base/increment", (_, res) => res.send("increment"));
+app.get("/base/decrement", (_, res) => res.send("decrement"));
+
+app.get("/shoulder/increment", (_, res) => res.send("increment"));
+app.get("/shoulder/decrement", (_, res) => res.send("decrement"));
+
+app.get("/elbow/increment", (_, res) => res.send("increment"));
+app.get("/elbow/decrement", (_, res) => res.send("decrement"));
+
+app.get("/grabber/increment", (_, res) => res.send("increment"));
+app.get("/grabber/decrement", (_, res) => res.send("decrement"));
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
